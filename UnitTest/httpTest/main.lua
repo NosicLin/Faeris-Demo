26e66fa2798b2befa8f6e53f242508aee6c16bcf
NosicLin <nosic@faeris.com>
2013-07-31 13:24:07 +0800

    @add http test

---
+++ UnitTest/httpTest/main.lua
@@ -0,0 +1,26 @@
+
+http=HttpEngine:create()
+
+
+request=HttpRequest:create()
+request:setUrl("www.baidu.com")
+
+request.data={                      
+	onReponse=function(code,data,err) 
+		--print("data"..data)
+		f_log("reponse:"..code);
+		--f_utillog("code=%d,data=%s,err=%s",code,data,err);
+		scene=Scene:create();
+		layer=Layer2D:create()
+		layer:setViewArea(0,0,960,640)
+		quad=ColorQuad2D:create(150,150,Color(255,0,0))
+		quad:setPosition(480,320)
+		layer:add(quad)
+		scene:push(layer)
+		share:director():run(scene)
+	end
+}
+f_log("send request");
+
+http:send(request)
+
